80cbbfaf3a3dbb44a8de38da0e0de29e2b34e60c
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2015-10-27 11:17:25 +0100

    rework chap 4, 5 and 6

---
+++ datasets/chapter6.R
@@ -0,0 +1,11 @@
+planets <- c("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+type <- c("Terrestrial planet", "Terrestrial planet", "Terrestrial planet", 
+          "Terrestrial planet", "Gas giant", "Gas giant", "Gas giant", "Gas giant")
+diameter <- c(0.382, 0.949, 1, 0.532, 11.209, 9.449, 4.007, 3.883)
+rotation <- c(58.64, -243.02, 1, 1.03, 0.41, 0.43, -0.72, 0.67)
+rings <- c(FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE)
+type_factor <- factor(type)
+planets_df <- data.frame(planets, type_factor, diameter, rotation, rings, stringsAsFactors = FALSE)
+names(planets_df) <- c("name", "type", "diameter", "rotation", "has_rings")
+rm(planets, type, diameter, rotation, rings, type_factor)
+save(planets_df, file = "datasets/chapter6.RData")
